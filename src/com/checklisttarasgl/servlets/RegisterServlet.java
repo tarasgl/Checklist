e1abc586f559b4ba8d825840f679c2edcf33b6e7
tarasgl <31354327+tarasgl@users.noreply.github.com>
2019-04-10 11:23:56 +0300

    Added registration

---
+++ src/com/checklisttarasgl/servlets/RegisterServlet.java
@@ -0,0 +1,95 @@
+package com.checklisttarasgl.servlets;
+
+import com.checklisttarasgl.classes.DBConnection;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@WebServlet(name = "RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String confirmPassword = request.getParameter("confirm-password");
+
+        if(username.isEmpty()||password.isEmpty()||confirmPassword.isEmpty()||
+                !confirmPassword.equals(password)){
+            request.setAttribute("fieldIsEmpty", true);
+            RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/register.jsp");
+            rd.include(request, response);
+        } else{
+            if(!confirmPassword.equals(password)){
+                request.setAttribute("passwordIsConfirmed", false);
+                RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/register.jsp");
+                rd.include(request, response);
+            } else {
+                //check if there is already existing username
+                try {
+                    String sqlGetCount = "SELECT COUNT(*) AS usernameCount FROM [User] WHERE username = ?";
+                    Connection conn = DBConnection.getConnection();
+                    PreparedStatement selectCountPrepStat = conn.prepareStatement(sqlGetCount);
+                    selectCountPrepStat.setString(1, username);
+                    ResultSet countResultSet= selectCountPrepStat.executeQuery();
+                    countResultSet.next();
+                    int countUsers = countResultSet.getInt("usernameCount");
+                    if(countUsers !=0){
+                        //user already exists, redirect to login again
+                        request.setAttribute("userAlreadyExists", true);
+                        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/register.jsp");
+                        rd.include(request, response);
+                    } else {
+                        //username is free, register
+                        try {
+                            String sqlRegisterUser = "INSERT INTO [User](username, password) " +
+                                    "VALUES (?,?) ";
+                            PreparedStatement addUserPrepStat = conn.prepareStatement(sqlRegisterUser);
+                            addUserPrepStat.setString(1,username);
+                            addUserPrepStat.setString(2,password);
+                            int usersAdded = addUserPrepStat.executeUpdate();
+                            if(usersAdded == 1){
+                                //user is registered. Log him in
+                                String sqlSelectUserId = "SELECT id FROM [User] WHERE username = ?";
+                                PreparedStatement getUserIdPrepStat = conn.prepareStatement(sqlSelectUserId);
+                                getUserIdPrepStat.setString(1, username);
+                                ResultSet userIdSet = getUserIdPrepStat.executeQuery();
+                                userIdSet.next();
+                                int userId = userIdSet.getInt(1);
+                                HttpSession session = request.getSession();
+                                session.setAttribute("userID", userId);
+                                session.setAttribute("username", username);
+                                //setting session to expiry in 10 mins
+                                session.setMaxInactiveInterval(10*60);
+
+                                response.sendRedirect("home");
+                            }
+                            else {
+                                //failed to register
+                            }
+
+                        } catch (SQLException ex){
+                            ex.printStackTrace();
+                        }
+                    }
+
+                } catch(SQLException ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/register.jsp");
+        rd.forward(request,response);
+    }
+}
--- web/WEB-INF/views/register.jsp
+++ web/WEB-INF/views/register.jsp
@@ -3,9 +3,7 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <html>
 <head>
-    <title>Login</title>
-    <!--Bootstrap -->
-
+    <title>Register</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
@@ -14,32 +12,40 @@
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
+    <nav class="navbar header navbar-expand-sm ">
+        <ul class="navbar-nav">
+            <li class="navbar-brand">Checklist</li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"> Register</a>
+            </li>
+        </ul>
+    </nav>
 
-<c:if test = "${(badLogin!= null && badLogin == true)} ">
-    <p>BadInput</p>
-</c:if>
-
-<nav class="navbar header navbar-expand-sm ">
-    <ul class="navbar-nav">
-        <li class="navbar-brand">Checklist</li>
-        <li class="nav-item">
-            <a class="nav-link" href="<c:url value="/register"/>"> Register</a>
-        </li>
-    </ul>
-</nav>
-
-<div class="row">
+    <div class="row">
     <div class = "col-lg-4 col-md-3 col-sm-2"></div>
     <div class ="col-lg-4 col-md-6 col-sm-8">
+        <h2>Register</h2>
         <form method = "post" >
+            <c:if test = "${fieldIsEmpty != null && fieldIsEmpty}">
+                Fill all Fields
+            </c:if>
+            <c:if test = "${userAlreadyExsists != null && userAlreadyExists}">
+                Username is taken
+            </c:if>
             Username:<br>
             <input type="text" name="username" value=""><br>
             Password:<br>
             <input type="password" name="password" value=""><br><br>
-            <input class ="btn  btn-primary" type="submit" value="Submit">
+
+            <c:if test = "${passwordIsConfirmed != null && !(passwordIsConfirmed)}">
+                Password is not confirmed
+            </c:if>
+            Confirm password:<br>
+            <input type="password" name="confirm-password" value=""><br><br>
+            <input class ="btn  btn-primary" type="submit" value="Register">
         </form>
     </div>
     <div class = "col-lg-4 col-md-3 col-sm-2"></div>
-</div>
+    </div>
 </body>
 </html>
